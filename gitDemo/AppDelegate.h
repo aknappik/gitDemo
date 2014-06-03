452ff06555ddeb819460ad5ec7b1f0c8d032ef4d
Alison Knappik <alison.knappik@d214.org>
2014-06-03 14:24:36 -0500

    Initial Commit

---
+++ gitDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitDemo
+//
+//  Created by Alison K. Knappik on 6/3/14.
+//  Copyright (c) 2014 aknapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ gitDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gitDemo
+//
+//  Created by Alison K. Knappik on 6/3/14.
+//  Copyright (c) 2014 aknapp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
